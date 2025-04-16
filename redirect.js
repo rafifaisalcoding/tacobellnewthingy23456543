3ab8de0a93b52c012bb1e5db00815c0b330d6ef7
rafifaisalcoding <rafifaisalawe@gmail.com>
2025-04-16 10:57:50 +0100

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,4 +1,3 @@
-
-function redirect() {
-  window.location.href = 'https://glstrck.com/aff_c?offer_id=655&aff_id=88356';
-}
+document.getElementById("startBtn").addEventListener("click", function() {
+    window.location.href = "https://glstrck.com/aff_c?offer_id=655&aff_id=88356";
+});
